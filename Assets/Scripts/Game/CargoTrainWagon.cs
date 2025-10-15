8d60ac1e20321dd33581908c39cd156dc98013bf
LucasBoe <lucas@lumotex.de>
2025-10-15 16:00:24 +0200

    ADDED: weight for wagons

--- Assets/Scripts/Game/CargoTrainWagon.cs
+++ Assets/Scripts/Game/CargoTrainWagon.cs
@@ -3,5 +3,9 @@ using UnityEngine;
 
 public class CargoTrainWagon : TrainWagonBase
 {
-    
+    protected override int CalculateWeight()
+    {
+        //add logic for contained cargo
+        return base.CalculateWeight();
+    }
 }
