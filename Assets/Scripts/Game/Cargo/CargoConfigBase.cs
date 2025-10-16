5a41e873d3e3a24a8cf72cda3487a05cee3212bb
ollulf <max.bredlau@googlemail.com>
2025-10-16 11:25:44 +0200

    ADDED: cargo wagons

--- Assets/Scripts/Game/Cargo/CargoConfigBase.cs
+++ Assets/Scripts/Game/Cargo/CargoConfigBase.cs
@@ -3,10 +3,14 @@ using UnityEngine;
 
 public class CargoConfigBase : ContaineableScriptableObject
 {
+    public enum CargoType {Small, Medium, Large}
+
+    [SerializeField] private CargoType type;
     [SerializeField] private Sprite sprite;
     [SerializeField] private int value;
     [SerializeField] private float weight;
 
+    public CargoType Type => type;
     public Sprite Sprite => sprite;
     public int Value => value;
     public float Weight => weight;
